7a01c67dd54796bfaeaa026d1f9243b1d943f64a
rohanmanatkar <rohanmanatkar@gmail.com>
2019-10-14 23:06:09 -0700

    first commit

---
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+import "./Node.css";
+
+export default class Node extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const {
+      col,
+      isFinish,
+      isStart,
+      isWall,
+      onMouseDown,
+      onMouseEnter,
+      onMouseUp,
+      row
+    } = this.props;
+    const extraClassName = isFinish
+      ? "node-finish"
+      : isStart
+      ? "node-start"
+      : //: isVisited
+      //? "node-visited"
+      isWall
+      ? "node-wall"
+      : "";
+    return (
+      <div
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}
+        onMouseDown={() => onMouseDown(row, col)}
+        onMouseEnter={() => onMouseEnter(row, col)}
+        onMouseUp={() => onMouseUp()}
+      ></div>
+    );
+  }
+}
